d725eef587c4c199c955ffacfb1ec29b8311f0aa
Kevin <airsen40@gmail.com>
2025-11-27 11:02:56 -0300

    First Commit

---
+++ src/main/java/com/sbur/RESTfulCoffee/ResTfulCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.sbur.RESTfulCoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResTfulCoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResTfulCoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sbur/RESTfulCoffee/ResTfulCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbur.RESTfulCoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResTfulCoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
